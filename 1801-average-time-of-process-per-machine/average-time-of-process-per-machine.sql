35c45e30292802687c6d470d70cab4e28686aff0
Srishareddy13 <srishareddys13@gmail.com>
2026-06-20 21:29:16 +0530

    Time: 240 ms (94.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,5 @@
+SELECT a1.machine_id,round(avg(a1.timestamp-a2.timestamp),3) as processing_time FROM Activity a1
+join Activity a2 on
+a1.machine_id=a2.machine_id and a1.process_id=a2.process_id and
+a1.activity_type='end' and a2.activity_type='start'
+group by a1.machine_id
